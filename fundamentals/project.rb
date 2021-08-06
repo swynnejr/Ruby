d02670d3bfd82de8216a070c294a5a76d1b135de
swynnejr <saucersam@gmail.com>
2021-08-05 21:39:20 -0500

    fixed

--- fundamentals/project.rb
+++ fundamentals/project.rb
@@ -1,15 +1,16 @@
 class Project
+    attr_reader :name, :description
     def initialize(name, description)
         @name = name
         @description = description
     end
-    def name()
+    def name_call
         puts "#{name}"
     end
-    def elevator_pitch()
+    def elevator_pitch
         puts "This is #{name}, and I #{description}."
     end
 end
 project1 = Project.new("Project 1", "Description 1")
-puts project1.name # => "Project 1"
+puts project1.name_call # => "Project 1"
 project1.elevator_pitch  # => "Project 1, Description 1"
