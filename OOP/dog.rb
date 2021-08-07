fc409669a6d652f680577e2486e5924dd40514e0
swynnejr <saucersam@gmail.com>
2021-08-06 22:16:40 -0500

    roar

--- OOP/dog.rb
+++ OOP/dog.rb
@@ -27,3 +27,26 @@ dog1.run
 dog1.pet
 dog1.display_health
 
+# CHECK SOLUTIONS says IF YOU RETURN SELF YOU CAN CHAIN
+# require_relative "mammal"
+
+# class Dog < Mammal
+#   def pet
+#     @health +=5
+#     self
+#   end
+
+#   def walk
+#     @health -=1
+#     self
+#   end
+  
+#   def run
+#     @health -=10
+#     self
+#   end
+# end
+
+# d = Dog.new
+# d.display_health
+# d.walk.walk.walk.run.run.pet.display_health
