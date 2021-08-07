8fe5d7cc9212a345f0741d99c051d58e08e8fc7c
swynnejr <saucersam@gmail.com>
2021-08-07 00:28:28 -0500

    with check solutions

--- OOP/bank_account.rb
+++ OOP/bank_account.rb
@@ -1,10 +1,13 @@
 class BankAccount
-    attr_accessor :account_number, :checking_account, :savings_account
+    # ONLY NEEDS READER, SINCE THESE SHOULDNT BE MANIPULATED
+    attr_reader :account_number, :checking_account, :savings_account
     @@count = 0
     def initialize
+        @account_number = generate_account
         @checking_account = 500
         @savings_account = 500
         @@count += 2
+        @interest_rate = 0.01
     end
     def checking_balance
         puts @checking_account
@@ -16,12 +19,33 @@ class BankAccount
     end
     def checking_deposit(amount)
         @checking_account += amount
-        self
     end
     def savings_deposit(amount)
         @savings_account += amount
-        self
     end
+# CHECK SOLUTIONS DID IT A COOL WAY
+    # def deposit(account, amount)
+    #     if account.downcase == "checking"
+    #     @checking += amount
+    #     else
+    #     @saving += amount 
+    #     end
+    # end
+    # def withdrawal(account, amount)
+    #     if account.downcase == "checking"
+    #       if @checking - amount < 0
+    #         raise "Insufficient Funds, you have #{@checking} in this account"
+    #       else
+    #         @checking -= amount
+    #       end
+    #     else
+    #       if @saving - amount < 0
+    #         raise "Insufficient Funds, you have #{@saving} in this account"
+    #       else
+    #         @saving -= amount
+    #       end
+    #     end
+    #   end
     def checking_withdrawl(amount)
         @checking_account -= amount
         if @checking_account < 0
@@ -37,15 +61,19 @@ class BankAccount
     def total_money
         total = @savings_account + @checking_account
         puts total
+    end
+    # def total
+    #     "Checking Balance: #{@checking}\nSaving Balance: #{@saving}\nTotal Balance: #{@checking + @saving}"
+    # end
+    # def account_information
+    #     "Account Number: #{@account_number}\nInterest Rate: #{@interest_rate}\n#{self.total}" 
+    # end
+    def self.number_of_accounts
+        puts @@count
+    end
     private
         def generate_account
             rng = Random.new
             @account_number = rng.rand(1000..10000)
-            puts @account_number
-            self
         end
 end
-
-account1 = BankAccount.new
-account1.savings_deposit(50)
-account1.savings_balance
