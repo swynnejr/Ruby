17f85a0e499b2ecd2bc3c20badf77d12ed20e355
swynnejr <saucersam@gmail.com>
2021-08-10 13:48:00 -0500

    inputs changed to hidden

--- rails/ninja_gold_game/app/controllers/games_controller.rb
+++ rails/ninja_gold_game/app/controllers/games_controller.rb
@@ -1,28 +1,33 @@
 class GamesController < ApplicationController
   # Making this "session[:counter]" is probably better than "@@counter"
-  @@counter = 0
-  
+  # session[:counter] = 0
+
   def index
-    flash[:notice] = "You have #{@@counter} gold!"
+    flash[:notice] = "You have #{session[:counter]} gold!"
+    session[:counter] = 0 unless session[:counter]
   end
 
   def goldmine
     # IF FORM SUBMITS FARM
     # if
-      @@counter += rand(10..20)
+      session[:counter] += rand(10..20)
     # IF FORM SUBMITS CAVE
     # elsif
-      @@counter += rand(5..10)
+      session[:counter] += rand(5..10)
     # IF FORM SUBMITS HOUSE
     # elsif
-      @@counter += rand(2..5)
+      session[:counter] += rand(2..5)
     # IF FORM SUBMITS CASINO
     # else
       gamble = rand(-50..50)
-      @@counter += gamble
+      session[:counter] += gamble
     # end
     redirect_to ''
   end
-  # IF I WANT TO MAKE A NEW CONTROLLER TO RESET SESSION
-  # DO I NEED TO: rails g controller Games restart
+
+  def restart
+    session[:counter] = 0
+    redirect_to ''
+  end
+
 end
