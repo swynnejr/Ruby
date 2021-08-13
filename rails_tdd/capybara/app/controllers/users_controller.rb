1171191448782e2ae7776c23841b5a057e36cdce
swynnejr <saucersam@gmail.com>
2021-08-13 16:51:22 -0500

    Capybara slayed!

--- rails_tdd/capybara/app/controllers/users_controller.rb
+++ rails_tdd/capybara/app/controllers/users_controller.rb
@@ -6,9 +6,19 @@ class UsersController < ApplicationController
     @user = User.new(params.require(:user).permit(:first_name, :last_name, :email))
     if @user.save
       flash[:notice] = ['User successfully created']
-      redirect_to new_user_path
+      redirect_to user_path @user.id
     else
-      #errors we need to code later
+      flash[:errors] = @user.errors.full_messages
+      redirect_to :back
     end
   end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:first_name, :last_name, :email)
+    end
 end
