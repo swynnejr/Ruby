e01cd1e75888400d954f4a4f3e3203f3cd432672
swynnejr <saucersam@gmail.com>
2021-08-14 15:52:19 -0500

    user updates and validates update

--- DojoSecrets/app/controllers/users_controller.rb
+++ DojoSecrets/app/controllers/users_controller.rb
@@ -25,10 +25,22 @@ class UsersController < ApplicationController
   end
 
   def edit
-  
+    @user = User.find(params[:id])
+
   end
 
   def update
+    user = User.find(params[:id])
+    user.update(user_params)
+    user.save
+    puts user.errors.full_messages if user.errors
+    if user.valid?
+      # redirect_to dojo_user_path(dojo_id: @user.dojo_id, id: @user.id)
+      redirect_to user_path(user)
+    else
+      flash[:notice] = user.errors.full_messages
+      redirect_to edit_user_path(user)
+    end
 
   end
   private
