9cc2cfe26ba31a4280349e40018a437d6f827094
swynnejr <saucersam@gmail.com>
2021-08-14 19:10:56 -0500

    Secrets listed, only user can delete

--- DojoSecrets/app/controllers/secrets_controller.rb
+++ DojoSecrets/app/controllers/secrets_controller.rb
@@ -1,5 +1,6 @@
 class SecretsController < ApplicationController
   def index
+    @secrets = Secret.all
   end
 
   def create
@@ -11,6 +12,11 @@ class SecretsController < ApplicationController
       flash[:notice] = @secret.errors.full_messages
       redirect_to :back
     end
+  end
+  def destroy
+    secret = Secret.find(params[:id])
+    secret.destroy
+    redirect_to :back
   end
   private
     def secret_params
