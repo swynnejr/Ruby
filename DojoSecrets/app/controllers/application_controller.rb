b41f83ed7ec996e0bf09a79abbe5cdcb8a2e0792
swynnejr <saucersam@gmail.com>
2021-08-14 22:09:23 -0500

    working on login authentication

--- DojoSecrets/app/controllers/application_controller.rb
+++ DojoSecrets/app/controllers/application_controller.rb
@@ -2,8 +2,23 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   # protect_from_forgery with: :exception
+
+  # before_action :require_login
+
   def current_user
     User.find(session[:user_id]) if session[:user_id]
   end
   helper_method :current_user
+
+
+  private
+
+  def require_login
+    unless logged_in?
+      flash[:error] = "You must be logged in to access this section"
+      redirect_to "/sessions/new" # halts request cycle
+    end
+  end
+
+
 end
