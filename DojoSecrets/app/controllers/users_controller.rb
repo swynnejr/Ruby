634b3587fec0da7b8d16c5eb6293a7bd6d57fafc
swynnejr <saucersam@gmail.com>
2021-08-18 17:23:21 -0500

    BELT REVIEW

--- DojoSecrets/app/controllers/users_controller.rb
+++ DojoSecrets/app/controllers/users_controller.rb
@@ -51,7 +51,7 @@ class UsersController < ApplicationController
   end
   private
     def user_params
-      params.require(:user).permit(:name, :email, :password)
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
 
     def verify_user
