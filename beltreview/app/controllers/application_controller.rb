9d141de088597a9587609e5477e3f2e8f2f27c8e
swynnejr <saucersam@gmail.com>
2021-08-19 00:14:08 -0500

    Wireframe layed out

--- beltreview/app/controllers/application_controller.rb
+++ beltreview/app/controllers/application_controller.rb
@@ -4,7 +4,11 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def current_user
-    User.find(session[:user_id]) if session[:user_id]
+    if session[:user_id]
+      User.find(session[:user_id])
+    else
+      nil
+    end
   end
   helper_method :current_user
 
--- beltreview/app/controllers/sessions_controller.rb
+++ beltreview/app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
     @user = User.find_by_email(params[:email])
     if @user && @user.authenticate(params[:password])
       session[:user_id] = @user.id
-      redirect_to "/users/#{@user.id}"
+      redirect_to "/events"
     else
       flash[:errors] = ["Invalid Combination"]
       redirect_to "/sessions/new"
